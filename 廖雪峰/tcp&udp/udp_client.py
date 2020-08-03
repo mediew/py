356a757b28b70b797d63be3f27e90502ec236bd0
mediew <2415018596@qq.com>
2020-08-03 23:44:51 +0800

    delete

--- 廖雪峰/tcp&udp/udp_client.py
+++
@@ -1,6 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-for data in [b'mecheal',b'tracy', b'sarah']:
-    s.sendto(data, ('127.0.0.1', 9999))
-    print(s.recvfrom(1024))
-s.close()
