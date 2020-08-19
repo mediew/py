57fc970dbe1d2f4ba3ade53bb0905eb480b2ad0c
mediew <2415018596@qq.com>
2020-08-19 23:11:51 +0800

    实例属性和类属性的绑定

--- python_advance/面向对象编程/实例属性和类属性.py
+++ python_advance/面向对象编程/实例属性和类属性.py
@@ -4,13 +4,17 @@ class Student(object):
     def __init__(self, name):
         self.name = name
 
-s = Student('Bob')     # 通过self变量
-s.score = 90           # 通过实例变量
+
+s = Student('Bob')  # 通过self变量，给实例绑定对象
+s.score = 90  # 通过实例变量,给实例绑定对象
+
 
 # 如果Student类本身需要绑定一个属性呢？可以直接在class中定义属性，这种属性是类属性，归Student类所有：
 
 class Student(object):
     name = 'Student'
+
+
 # 当我们定义了一个类属性后，这个属性虽然归类所有，但类的所有实例都可以访问到。来测试一下：
 '''
 >>> class Student(object):
@@ -31,8 +35,9 @@ Student
 Student
 从上面的例子可以看出，在编写程序的时候，千万不要对实例属性和类属性使用相同的名字，因为相同名称的实例属性将屏蔽掉类属性，但是当你删除实例属性后，再使用相同的名称，访问到的将是类属性。
 '''
-# 练习
-# 为了统计学生人数，可以给Student类增加一个类属性，每创建一个实例，该属性自动增加：
+
+
+# 练习：为了统计学生人数，可以给Student类增加一个类属性，每创建一个实例，该属性自动增加：
 class Student(object):
     count = 0
 
