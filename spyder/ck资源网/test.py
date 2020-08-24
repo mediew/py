34fa7054087fdd99478d07f2caf0d10c7c272427
mediew <2415018596@qq.com>
2020-08-24 16:55:01 +0800

    test 多进程

--- spyder/ck资源网/test.py
+++ spyder/ck资源网/test.py
@@ -1,6 +1,15 @@
 import ffmpy3
+from multiprocessing import Pool
 
 
-name = './test.mp4'
-link = 'https://daqqzz.com/20200527/eec17eb73eba3ee8cf227271a89c4f6c.mp4/index.m3u8?ts=1598248865000&token=74332d603a40447ab8e78387db50eb55'
-ffmpy3.FFmpeg(inputs={link: None}, outputs={name:None}).run()
+def main(name, link):
+    ffmpy3.FFmpeg(inputs={link: None}, outputs={name: None}).run()
+
+
+if __name__ == '__main__':
+    name = './test.mp4'
+    link = 'https://daqqzz.com/20200118/a187bf139b8fe6452130d28921c4b5cf.mp4/index.m3u8?ts=1598258626000&token=1ccf90cce80953842dc75a60f865a0ba'
+    p = Pool(8)
+    p.apply_async(main, args=(name, link))
+    p.close()
+    p.join()
