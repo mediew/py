fa4c79ed9a40b96027235a87af32bf65db7c65e1
mediew <2415018596@qq.com>
2020-09-04 17:08:11 +0800

    optimize this programme

--- 显示当前时间.py
+++ 显示当前时间.py
@@ -1,9 +1,10 @@
 import time
-currentTime=time.time()
-totalSeconds=int(currentTime)
-currentSeconds=totalSeconds%60
-totalMinutes=totalSeconds//60
-currrentMinutes=totalMinutes%60
-totalHours=totalMinutes//60
-currentHour=totalHours%24
-print("current time is", currentHour, ":", currrentMinutes, ":",currentSeconds, "GMT")
+
+currentTime = time.time()
+totalSeconds = int(currentTime)
+currentSeconds = totalSeconds % 60
+totalMinutes = totalSeconds // 60
+currrentMinutes = totalMinutes % 60
+totalHours = totalMinutes // 60
+currentHour = totalHours % 24
+print("current time is", currentHour, ":", currrentMinutes, ":", currentSeconds, "GMT")
